590d862deadea893d7dcecf46d3de9876ac1cc89
Debliqui Valentin <debliqui.valentin@outlook.com>
2025-03-24 16:18:17 -0400

    Adding a container for information popUp

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import CollapseArticles from "./container/CollapseArticles"
 import "./App.scss"
 import Earth from "./assets/PIA18033.svg"
 import Slider from "./container/Slider"
+import PopUp from "./container/PopUp"
 
 function App() {
   const { theme } = useContext(ThemeContext)
@@ -19,15 +20,22 @@ function App() {
     <main className="main">
       <img src={Earth} alt="" className="main__earth" />
       <header className="main__header">
-        <h1>Terre 1</h1>
-        <h2>Manuel des nouveaux arrivants</h2>
+        <div className="main__header__content">
+          <h1>Terre 1</h1>
+          <h2>Manuel des nouveaux arrivants</h2>
+        </div>
       </header>
-      <section>
+      <section className="main__navigation">
+        <nav className="nav">
+          <PopUp />
+        </nav>
         <CollapseArticles />
       </section>
       <section className="main__bottom">
-        <Slider />
-        <Settings />
+        <div className="main__bottom__content">
+          <Slider />
+          <Settings />
+        </div>
       </section>
     </main>
   )
